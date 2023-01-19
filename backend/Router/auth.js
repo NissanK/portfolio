eda2782045e1f293254ded8b64b2e573a143e787
Nissan Kumar <nissankumar9001@gmail.com>
2023-01-20 03:26:56 +0530

    backend initialized

---
+++ backend/Router/auth.js
@@ -0,0 +1,26 @@
+const { application } = require('express');
+const express = require('express');
+const router = express.Router();
+
+require('../db/connection');
+const Form = require('../schemas/formSchema')
+
+router.get('/',(req,res)=>{
+    res.send('Hello world from the server router yeet');
+})
+
+router.post('/',(req,res)=>{
+    const {name,email,message} = req.body;
+    if(!name || !email || !message){
+        return res.status(422).json({error : "Please fill all the fields"});
+    }
+    
+    const form = new Form({name,email,message});
+    form.save().then(()=>{
+        res.status(201).json({message : "Form sent successfully"});
+    }).catch((err) =>{
+        res.status(500).json({error : "Failed to send the form"});
+    })
+})
+
+module.exports = router;
---
+++ frontend/components/Contact/ContactIndex.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionHeading from '../Universal/SectionHeading'
+
+function ContactIndex() {
+  return (
+    <div>
+        <SectionHeading heading="Contact"></SectionHeading>
+    </div>
+  )
+}
+
+export default ContactIndex
