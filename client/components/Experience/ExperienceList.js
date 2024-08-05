fa9afe6af0cec812a7857def5378fc3490e39190
Nissan Kumar <nissankumar9001@gmail.com>
2024-08-05 20:29:44 +0530

    Update ExperienceList.js

--- client/components/Experience/ExperienceList.js
+++ client/components/Experience/ExperienceList.js
@@ -5,10 +5,10 @@ const ExperienceList =
         role : "Software Development Engineering Intern",
         timeline : {start : "May 2024", end : "July 2024"},
         keypoints : [
-            "Implemented a function to segregate failed invoices into specific Dead Letter Queues, for efficient re-drive processes.",
-            "Prioritized the validation of small invoices promptly during high traffic, preventing delays caused by large invoices in the queues further improving failed invoices re-drive time by 50%.",
-            "Designed APIs using Design Patterns to validate end-to-end triggering functions for invoices, resulting in a 25% reduction in processing errors and achieving 95% code coverage through comprehensive unit tests.",
-            "Tech Stack: Java, Junit, Mockito, Git, Smithy, AWS"
+            "Created a function to segregate failed invoices into specific Dead Letter Queues by prioritizing validation of small invoices during high traffic, preventing starvation by large invoices and improving re-drive time for failed invoices by 50%.",
+            "Designed APIs using Design Patterns to validate end-to-end triggering functions for invoices, reducing processing errors by 25% and achieving 100% code coverage through comprehensive unit tests.",
+            "Developed and executed integration tests for seamless service interaction. Utilized AWS services like SQS, SNS, Step Functions, SWF, DDB, S3, Route53, and CloudWatch throughout the internship.",
+            "Tech Stack: Tech Stack: Java, Junit, Mockito, Git, Smithy, AWS"
         ],
         link : null,
         expId : 0
